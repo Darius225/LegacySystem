5553623c32a944379febf9258216129fad97c294
Darius225 <darius.grigore.pop10@gmail.com>
2017-12-01 20:21:01 +0000

    Refactored the tests again.
    Made the code nicer

--- Oyster/test/com/tfl/billing/TravelTrackerTest.java
+++ Oyster/test/com/tfl/billing/TravelTrackerTest.java
@@ -21,101 +21,94 @@ import static org.junit.Assert.assertThat;
 
 
 
-public class TravelTrackerTest 
+public class TravelTrackerTest
 {
 
     @Rule
     public final ExpectedException exception = ExpectedException.none();
-
-    @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+    private final OysterCard myCard = new OysterCard("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+    private final TravelTracker mockTracker = Mockito.spy(new TravelTracker());
+    private final IdentificationReader victoriaReader = new OysterCardIDReader(Station.VICTORIA_STATION);
+    private boolean[] travelling = new boolean [ 2 ] ;
+    private final OysterCardIDReader paddingtonReader = new OysterCardIDReader("38400000-8cf0-11bd-b23e-10b96e4ef10d");
+    private final OysterCardIDReader barbicanReader = new OysterCardIDReader("38402800-8cf0-11bd-b23e-10b96e4ef00d");
+    private final OysterCard nonExistentCard = new OysterCard("38400000-8cf0-11bd-b23e-10b96e4ef00e") ;
+    private final OysterCardReader paddingtonMockReader = Mockito.spy(OysterReaderLocator.atStation(Station.PADDINGTON));
+    private ControllableCustomerDatabase database = new ControllableCustomerDatabase();
+    private MockBillingSystem system = new MockBillingSystem();
+    private ControllableClock clock = new ControllableClock();
+    private Cache c = new Cache () ;
+    private ArrayList <OysterCardIDReader> readers = new ArrayList<>() ;
+    @Rule
+    public JUnitRuleMockery context = new JUnitRuleMockery();
     @Test
-    public void connect() throws Exception 
+    public void connect() throws Exception
     {
-        TravelTracker tracker = Mockito.spy(new TravelTracker());
-        OysterCardReader reader = Mockito.spy(OysterReaderLocator.atStation(Station.PADDINGTON));
-        tracker.connect(reader);
-        Mockito.verify(reader).register(tracker);
+
+        mockTracker.connect(paddingtonMockReader);
+        Mockito.verify(paddingtonMockReader).register(mockTracker);
     }
     @Test
     public void cardScannedOnce() throws Exception
     {
-        TravelTracker tracker = Mockito.spy(new TravelTracker());
-        OysterCard myCard = new OysterCard("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-        IdentificationReader reader = new OysterCardIDReader(Station.VICTORIA_STATION);
-        tracker.connect(reader);
-        reader.touch(myCard);
-        Mockito.verify(tracker).cardScanned(myCard.id(),reader.id());
-        assertThat(tracker.isTraveling(myCard.id()),is(true));
+        cardScannedSomeNumberOfTimes( 1 );
     }
     @Test
     public void cardScannedTwice() throws Exception
     {
-        TravelTracker tracker = Mockito.spy(new TravelTracker());
-        OysterCard myCard = new OysterCard("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-        IdentificationReader reader = new OysterCardIDReader(Station.VICTORIA_STATION);
-        tracker.connect(reader);
-        reader.touch(myCard);
-        reader.touch(myCard);
-        Mockito.verify(tracker,Mockito.times(2)).cardScanned(myCard.id(),reader.id());
-        assertThat(tracker.isTraveling(myCard.id()),is(false));
+        cardScannedSomeNumberOfTimes( 2 );
     }
     @Test
     public void unknownCardScanned() throws Exception
     {
-        TravelTracker tracker = Mockito.spy(new TravelTracker());
-        OysterCard myCard = new OysterCard("38400332-8cf0-11bd-b23e-10b96e4ef00d");
-        IdentificationReader reader = new OysterCardIDReader(Station.VICTORIA_STATION);
-        tracker.connect(reader);
-
+        mockTracker.connect(victoriaReader);
         exception.expect(UnknownOysterCardException.class);
-
-        reader.touch(myCard);
-
+        victoriaReader.touch(nonExistentCard);
     }
 
     @Test
     public void chargeNothingIfNoJourneysMade() throws Exception
     {
-        no_journey ( ) ;
+        noJourney ( ) ;
     }
     @Test
     public void chargeIfJourneyIsMade() throws Exception
     {
-        single_journey ( 10 , 0 , 15 , 2.40 ) ;
+        singleJourney ( 10 , 0 , 15 , 2.40 ) ;
     }
     @Test
     public void chargePeakIfPeakJourneyIsMade() throws Exception
     {
-        single_journey ( 9 ,  0 , 15 , 3.20 ) ;
+        singleJourney ( 9 ,  0 , 15 , 3.20 ) ;
     }
     @Test
     public void chargeLongIfLongJourneyIsMade() throws Exception
     {
-        single_journey ( 10 , 0 , 30 , 2.70 );
+        singleJourney ( 10 , 0 , 30 , 2.70 );
     }
     @Test
-    public void chargeLongPeakIfLongPeakJourneyIsMade() throws Exception 
+    public void chargeLongPeakIfLongPeakJourneyIsMade() throws Exception
     {
-        single_journey ( 9  , 0 , 30 , 3.80 ) ;
+        singleJourney ( 9  , 0 , 30 , 3.80 ) ;
     }
     @Test
     public void capOffPeak()
     {
-        charge_Sequence_Of_Events ( 6 , new int [ ] { 10 , 10 , 10 , 11 , 11 , 11 } , new int [ ] { 00 , 15 , 55 , 15 , 20 , 55 } , 7.00 );
+        chargeSequenceOfEvents ( 6 , new int [ ] { 10 , 10 , 10 , 11 , 11 , 11 } , new int [ ] { 00 , 15 , 55 , 15 , 20 , 55 } , 7.00 );
     }
     @Test
     public void capPeak()
     {
-        charge_Sequence_Of_Events ( 6 , new int [ ] {  9 ,  9 ,  9 , 10 , 10 , 10 } , new int [ ] { 00 , 15 , 55 , 15 , 20 , 55 } , 9.00 );
+        chargeSequenceOfEvents ( 6 , new int [ ] {  9 ,  9 ,  9 , 10 , 10 , 10 } , new int [ ] { 00 , 15 , 55 , 15 , 20 , 55 } , 9.00 );
     }
 
-    private class ControllableClock implements Clock 
+    private class ControllableClock implements Clock
     {
 
         LocalTime time = LocalTime.now();
 
         @Override
-        public long getNow() 
+        public long getNow()
         {
             return (long)(time.toNanoOfDay()/1e6);
         }
@@ -125,33 +118,25 @@ public class TravelTrackerTest
             time = LocalTime.of(hour-1,minute,0,0);
         }
     }
-    public void no_journey( ) // If no journey is made,then there are 0 events .
+    public void noJourney( ) // If no journey is made,then there are 0 events .
     {
-           charge_Sequence_Of_Events( 0 , new int [ ] {} , new int [ ] {} , 0 );
+        chargeSequenceOfEvents( 0 , new int [ ] {} , new int [ ] {} , 0 );
     }
-    public void single_journey ( int start_Hour , int start_Minute , int length , double expected_Total ) // A journey can be seen as a sequence of 2 events .
+    public void singleJourney ( int startHour , int startMinute , int length , double expectedTotal ) // A journey can be seen as a sequence of 2 events .
     {
-           int end_Minute = start_Minute + length ; // Determine the end minute and hour of the journey.We should take into account the case when the end hour is different from the start hour .
-           int end_Hour = start_Hour + end_Minute / 60 ;
-           end_Minute = end_Minute % 60 ;
-           charge_Sequence_Of_Events ( 2 , new int[] { start_Hour , end_Hour } , new int[] { start_Minute , end_Minute  } , expected_Total ) ;
+        int endMinute = startMinute + length ; // Determine the end minute and hour of the journey.We should take into account the case when the end hour is different from the start hour .
+        int endHour = startHour + endMinute / 60 ;
+        endMinute = endMinute % 60 ;
+        chargeSequenceOfEvents ( 2 , new int[] { startHour , endHour } , new int[] { startMinute , endMinute  } , expectedTotal ) ;
     }
-    public void charge_Sequence_Of_Events ( int no_events , int hour [ ] , int minute [ ] , double expected_Total )
+    public void chargeSequenceOfEvents ( int noEvents , int hour [ ] , int minute [ ] , double expectedTotal )
     {
-        ControllableCustomerDatabase database = new ControllableCustomerDatabase();
-        OysterCard myCard = new OysterCard("38400000-8cf0-11bd-b23e-10b96e4ef00d");
         database.add(new Customer("John Smith",myCard));
-
-        OysterCardIDReader paddingtonReader = new OysterCardIDReader("38400000-8cf0-11bd-b23e-10b96e4ef10d");
-        OysterCardIDReader barbicanReader = new OysterCardIDReader("38402800-8cf0-11bd-b23e-10b96e4ef00d");
-        MockBillingSystem system = new MockBillingSystem();
-        ControllableClock clock = new ControllableClock();
-        Cache c = new Cache () ;
         JourneyBuilder builder = new JourneyBuilder(clock,database,c );
-        ArrayList <OysterCardIDReader > readers = new ArrayList< >();
+
         readers.add ( paddingtonReader ) ;
         readers.add ( barbicanReader ) ;
-        for ( int i = 0 ; i < no_events ; i ++ )
+        for ( int i = 0 ; i < noEvents ; i ++ )
         {
             clock.setTIme ( hour [ i ] , minute  [ i ] ) ;
             builder.addEvent ( myCard , readers.get ( i % 2 ) ) ;
@@ -159,7 +144,18 @@ public class TravelTrackerTest
         TravelTracker tracker = new TravelTracker(system,database,builder.getEventLog(),c);
 
         tracker.chargeAccounts();
-        assertThat(system.getTotalBill(),is(new BigDecimal(expected_Total).setScale(2, BigDecimal.ROUND_HALF_UP)));
+        assertThat(system.getTotalBill(),is(new BigDecimal(expectedTotal).setScale(2, BigDecimal.ROUND_HALF_UP)));
     }
-
+    private void cardScannedSomeNumberOfTimes ( int noTimes ) throws Exception
+    {
+        travelling [ 0 ] = false ;
+        travelling [ 1 ] = true ;
+        mockTracker.connect ( victoriaReader );
+        for ( int i = 1 ; i <= noTimes ; i ++ )
+        {
+            victoriaReader.touch(myCard);
+        }
+        Mockito.verify(mockTracker,Mockito.times(noTimes)).cardScanned(myCard.id(),victoriaReader.id());
+        assertThat(mockTracker.isTraveling(myCard.id()),is( travelling [ noTimes % 2 ] ));
+    }
 }
